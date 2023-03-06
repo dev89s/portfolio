27354739a427f02fc5eef92575c33218a0d84a09
Sasan Moshirabadi <sasan.dev89@gmail.com>
2023-03-06 19:36:24 +0300

    Fix Eslint errors, const variables, single quote

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
-let hamburgerButton = document.querySelector("#hamburger-menu");
+const hamburgerButton = document.querySelector("#hamburger-menu");
 
-let overlayMenu = document.querySelector(".overlay-menu");
+const overlayMenu = document.querySelector(".overlay-menu");
 
-let close = document.querySelector(".close");
+const close = document.querySelector(".close");
 
-let portfolio = document.querySelector("#portfolio-link");
-let about = document.querySelector("#about-link");
-let contact = document.querySelector("#contact-link");
+const portfolio = document.querySelector("#portfolio-link");
+const about = document.querySelector("#about-link");
+const contact = document.querySelector("#contact-link");
 
 hamburgerButton.onclick = function () {
-  overlayMenu.style.display = "block";
+  overlayMenu.style.display = 'block';
 };
 
 close.onclick = function () {
-  overlayMenu.style.display = "none";
+  overlayMenu.style.display = 'none';
 }
 
 portfolio.onclick = function () {
-  overlayMenu.style.display = "none";
+  overlayMenu.style.display = 'none';
 };
 
 about.onclick = function () {
-  overlayMenu.style.display = "none";
+  overlayMenu.style.display = 'none';
 };
 
 contact.onclick = function () {
-  overlayMenu.style.display = "none";
+  overlayMenu.style.display = 'none';
 };
