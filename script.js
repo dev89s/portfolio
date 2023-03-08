3e232a56ce9dbe4ba8e9e1d9a6d6e13ba5da5085
Sasan Moshirabadi <sasan.dev89@gmail.com>
2023-03-08 18:15:51 +0300

    Add desktop popup card modal, change html arrangement, add styling

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ const cardInfo = [
     company: 'CANOPY',
     role: 'Back End Dev',
     year: '2015',
-    desctiption: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    desctiption: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a thing.",
     techstack: ['html', 'css', 'javasctipt'],
     imageUrl: "src/images/card-cover1.png"
   },
@@ -47,7 +47,7 @@ const cardInfo = [
     company: 'CANOPY',
     role: 'Back End Dev',
     year: '2015',
-    desctiption: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    desctiption: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a thing.",
     techstack: ['html', 'css', 'javasctipt'],
     imageUrl: "src/images/card-cover2.png"
   },
@@ -56,7 +56,7 @@ const cardInfo = [
     company: 'CANOPY',
     role: 'Back End Dev',
     year: '2015',
-    desctiption: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    desctiption: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a thing.",
     techstack: ['html', 'css', 'javasctipt'],
     imageUrl: "src/images/card-cover3.png"
   },
@@ -65,7 +65,7 @@ const cardInfo = [
     company: 'CANOPY',
     role: 'Back End Dev',
     year: '2015',
-    desctiption: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    desctiption: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a thing.",
     techstack: ['html', 'css', 'javasctipt'],
     imageUrl: "src/images/card-cover4.png"
   }
@@ -83,35 +83,39 @@ function generate_popup(cardNum) {
   cardModalContainer.style.display = 'block';
   const info = cardInfo[cardNum];
   cardModal.innerHTML = `
-  <div class="card card1">
-    <div class="modal-title-container">
-      <h2 class="card-title">${info.name}</h2>
-      <button type="button"class="close-modal">&times;</button>
-    </div>  
-    <ul class="card-details">
-      <li class="prim-detail-cap">${info.company}</li>
-      <li><img src="src/icons/counter.svg" alt="circle dots"></li>
-      <Li class="sec-detail-cap">${info.role}</Li>
-      <li><img src="src/icons/counter.svg" alt="circle dots"></li>
-      <li class="sec-detail-cap">${info.year}</li>
-    </ul>
+  <div class="card card${cardNum + 1} popup-card">
+    <div class="popup-header">
+      <div class="modal-title-container">
+        <h2 class="card-title">${info.name}</h2>
+        <button type="button"class="close-modal">&times;</button>
+      </div>  
+      <ul class="card-details">
+        <li class="prim-detail-cap">${info.company}</li>
+        <li><img src="src/icons/counter.svg" alt="circle dots"></li>
+        <Li class="sec-detail-cap">${info.role}</Li>
+        <li><img src="src/icons/counter.svg" alt="circle dots"></li>
+        <li class="sec-detail-cap">${info.year}</li>
+      </ul>
+    </div> 
     <img class="card-cover" src="${info.imageUrl}" alt="image of previous work example">
     <div class="card-info">
       <p class="description">
        ${info.desctiption}
       </p>
-      <ul class="techstack">
-        <li>${info.techstack[0]}</li>
-        <li>${info.techstack[1]}</li>
-        <li>${info.techstack[2]}</li>
-      </ul>
-      <div class="popup-buttons">
-        <button class="card-button" type="button" id="modalBtn1">See Live
-          <img src="src/icons/see-live.svg">
-        </button>
-        <button class="card-button" type="button" id="modalBtn2">See Source
-          <img src="src/icons/see-source-github.svg">
-        </button>
+      <div class="card-info-sub">
+        <ul class="techstack">
+          <li>${info.techstack[0]}</li>
+          <li>${info.techstack[1]}</li>
+          <li>${info.techstack[2]}</li>
+        </ul>
+        <div class="popup-buttons">
+          <button class="card-button" type="button" id="modalBtn1">See Live
+            <img src="src/icons/see-live.svg">
+          </button>
+          <button class="card-button" type="button" id="modalBtn2">See Source
+            <img src="src/icons/see-source-github.svg">
+          </button>
+        </div>
       </div>
     </div>
   </div>
@@ -135,3 +139,9 @@ thirdCardBtn.addEventListener('click', () => {
 forthCardBtn.addEventListener('click', () => {
   generate_popup(3);
 });
+
+window.onclick = function(event) {
+  if (event.target == cardModalContainer) {
+    cardModalContainer.style.display = "none";
+  }
+} 
